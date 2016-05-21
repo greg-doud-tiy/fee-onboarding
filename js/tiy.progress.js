807660380d128413cf76311d79b4ab1bab116cd6
Jordan Kasper <jordan@jordankasper.com>
2016-05-21 15:56:45 -0400

    Added collection of - and display of - student's name
    The name is stored in localStorage as well as the progress through the sections. We can use this name during final survey for submission, but it also is helpful to know when a new person is on the site.

--- js/tiy.progress.js
+++ js/tiy.progress.js
@@ -3,10 +3,18 @@
 
     var progress, content;
     var PROGRESS_KEY = 'section-progress';
+    var NAME_KEY = 'tiy-name';
 
     init();
 
     function init() {
+        var name = localStorage.getItem(NAME_KEY);
+        if (name) {
+            document.querySelector('.student-name').innerText = `Hello again ${name}!`;
+        } else {
+            askForName();
+        }
+
         content = document.querySelector('.content');
 
         try { progress = JSON.parse(localStorage.getItem(PROGRESS_KEY)) || {}; } catch(e){ progress = {}; }
@@ -61,6 +69,42 @@
             }
             if (progress[section.innerText]) {
                 section.innerHTML += "<span class='is-complete'>&#10003;</span>";
+            }
+        });
+    }
+
+    function askForName() {
+        var modal = `<div class='modal-background'></div>
+<aside class='modal name-entry'>
+    <h3>Hello!</h3>
+    <p>
+        It looks like we haven't met yet. Can you introduce yourself?
+        This website will track your progress through the onboarding
+        process for The Iron Yard. At the end of these sections you
+        will be asked to submit a final onboarding survey which will
+        send your name (below) along with your progress.
+    </p>
+    <form>
+        <label for='student_name'>My name is:</label>
+        <input type='text' id='student_name' autofocus>
+        <input type='submit' class='save-name btn' value='Save'>
+    </form>
+</aside>`;
+
+        var content = document.createElement('section');
+        content.classList.add('name-entry-modal');
+        content.innerHTML = modal;
+
+        document.body.appendChild(content);
+        document.querySelector('.modal-background').style.height = document.body.clientHeight + 'px';
+
+        document.querySelector('.name-entry form').addEventListener('submit', function(e) {
+            e.preventDefault();
+            var name = document.getElementById('student_name').value;
+            console.log('saving name', name);
+            if (name && name.length) {
+                localStorage.setItem(NAME_KEY, name);
+                document.body.removeChild(document.querySelector('.name-entry-modal'));
             }
         });
     }
